3c70484b3f1ec2f5a87e4e91d2bd08282e5b4990
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-17 14:00:45 +0200

    Home page done

--- react/my-app/src/Home.jsx
+++ react/my-app/src/Home.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { getFullYear } from "./utils";
+import "./Home.css";
 
 export default class MainContent extends Component {
   render() {
@@ -7,13 +8,12 @@ export default class MainContent extends Component {
       <div className="container">
         <h1 className="header">KigaliHomes</h1>
         <p className="paragraph">
-          {" "}
           Welcome to KigaliHomes!
           <br />
           Jump right in and explore our many houses. <br />
-          Feel free to share some of your own and comment on others!{" "}
+          Feel free to share some of your own <br /> and comment on others!{" "}
         </p>
-        <button class="btn btn-primary">View Houses</button>
+        <button className=" clickme">View Houses</button>
         <div className="App-footer">
           <p>Copyright {getFullYear()}</p>
         </div>
