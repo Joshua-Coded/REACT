69aa2a3753804db49d5e9b36540d2803d0c2fdb2
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-05-17 10:56:53 +0200

    installed bootstrap popper and jquery

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div>Hello World!
+    <div>
+      <button className="btn btn-danger"> Hello World!</button>
      </div>
   );
 }
